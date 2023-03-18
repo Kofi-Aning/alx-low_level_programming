7a876ceb4b7fa106d07ee89581ae6f3f630383e4
Kofi-Aning <aningpapakofi1@gmail.com>
2023-03-17 23:24:51 -0700

    update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-  *Main - Intial Point
-  *return 0 as success
+  *main - Intial Point
+  *return: 0 as(success)
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
